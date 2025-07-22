7487131a008f5602c02e0403ec7bc04cd25f15cd
Matei Stanca <i@ambientimpact.com>
2025-07-21 21:53:25 -0400

    Reworked tooltip RefreshLess cache handling to act on cached snapshot.

--- javascript/tooltip.title_attribute.js
+++ javascript/tooltip.title_attribute.js
@@ -52,27 +52,10 @@ AmbientImpact.addComponent('OmnipediaSiteThemeTooltipTitleAttribute', function(
     'OmnipediaSiteThemeTooltipTitleAttribute',
     'omnipedia-site-theme-tooltip-title-attribute',
     '.layout-container',
+    // We want to ignore 'refreshless:before-cache' but we do want to be
+    // notified when displaying a cached snapshot.
+    ['unload', 'refreshless:cached-snapshot'],
     function(context, settings) {
-
-      // Restore any title attributes that were left as data attributes as can
-      // occur when restoring from RefreshLess' cache.
-      //
-      // @todo Remove when we can reliably detach before caching.
-      $(this).find(
-        `[data-original-title]:not(${attachedDataLinkSelector})`,
-      ).each(async (i, element) => {
-
-        const $this = $(element);
-
-        await fastdom.mutate(() => {
-
-          $this.attr('title', $this.attr('data-original-title')).removeAttr(
-            'data-original-title',
-          );
-
-        });
-
-      });
 
       $(this).prop(
         propertyName,
@@ -83,6 +66,34 @@ AmbientImpact.addComponent('OmnipediaSiteThemeTooltipTitleAttribute', function(
 
     },
     function(context, settings, trigger) {
+
+      // Remove any cached element and return if displaying a cached snapshot.
+      if (trigger === 'refreshless:cached-snapshot') {
+
+        // Restore any title attributes that were left as data attributes as can
+        // occur when restoring from RefreshLess' cache.
+        $(this).find('[data-original-title]').each(async (i, element) => {
+
+          const $this = $(element);
+
+          await fastdom.mutate(() => {
+
+            $this.attr('title', $this.attr('data-original-title')).removeAttr(
+              'data-original-title',
+            );
+
+            // Find the Tooltip itself, if it exists, and remove it as well.
+            $(`#${$this.attr('aria-describedby')}`).remove();
+
+            $this.removeAttr('aria-describedby');
+
+          });
+
+        });
+
+        return;
+
+      }
 
       $(this).prop(propertyName)?.destroy();
 
